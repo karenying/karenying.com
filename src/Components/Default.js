9e74d5227049a3db3e3dc47457efb147096a921f
Karen Ying <karenying7@gmail.com>
2020-02-20 16:51:29 -0500

    Initial commit

---
+++ src/Components/Default.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Component extends React.Component {
+
+    render() {
+        
+        return (
+            <>
+                <div>
+                    
+                </div>
+            </>
+        );
+    }
+}
+
+export default Component;
---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+    render() {
+        
+        return (
+            <>
+                <div>
+                    
+                </div>
+            </>
+        );
+    }
+}
+
+export default Home;
