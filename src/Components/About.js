db792c76a0aa542b5d41f0a62878a494159205a4
Karen Ying <karenying7@gmail.com>
2020-02-27 13:24:32 -0500

    Style about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../Styles/About.css';
 import Header from './Header';
+import Karen from '../Assets/karen.png'
 
 class About extends React.Component {
 
@@ -10,6 +11,26 @@ class About extends React.Component {
             <>
                 <div className="about-container">
                     <Header page="about"/>
+                    <div className="about-content-container">
+                        <div className="about-content-left">
+                            <img src={Karen}></img>
+                        </div>
+                        <div className="about-content-right">
+                            <div>
+                                <h1>hello!</h1>
+                                <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                                sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </>
         );
