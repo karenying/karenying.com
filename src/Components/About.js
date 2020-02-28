21309acb2a82a657c465048163c40ae79cd8d153
Karen Ying <karenying7@gmail.com>
2020-02-28 15:07:21 -0500

    Modify about format

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,13 +19,9 @@ class About extends React.Component {
                             <div>
                                 <h1>hello!</h1>
                                 <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-                                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    I'm a junior studying Computer Science at Princeton University.
+                                    Welcome to my own little space on the web 🙂. 
+                                    
 
                                 </p>
                             </div>
