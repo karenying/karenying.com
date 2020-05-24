5cbde437c31e1fb30fe75959c116f88cf685ba31
Karen Ying <karenying7@gmail.com>
2020-05-24 12:17:02 -0400

    Add project info

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,24 +1,78 @@
 import React from 'react';
 import '../Styles/Projects.css';
 import Header from './Header';
-import Expresso from '../Assets/expresso.png';
-import Dayz from '../Assets/dayz.png';
-import { DARK_PINK, LIGHT_PINK, BLUE, YELLOW, LIGHT_GRAY } from '../constants';
+import {
+    DARK_PINK,
+    LIGHT_PINK,
+    BLUE,
+    YELLOW,
+    LIGHT_GRAY,
+    projectInfo,
+} from '../constants';
 import { withStyles } from '@material-ui/core/styles';
 import { Card } from '@material-ui/core';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Gradient from '../Assets/gradient.png';
+
 import { FiExternalLink } from 'react-icons/fi';
 import { FaCode } from 'react-icons/fa';
 
 class Projects extends React.Component {
+    state = {
+        index: 0,
+    };
+
     render() {
+        const { index } = this.state;
+        const currInfo = projectInfo[index];
+        const {
+            backgroundColor,
+            color,
+            image,
+            name,
+            type,
+            stack,
+            description,
+            site,
+            code,
+        } = currInfo;
+
+        const buttonMargin = code ? 30 : 0;
+        const siteButton = site ? (
+            <a href={site}>
+                <div
+                    className='projectcard-site'
+                    style={{
+                        marginRight: buttonMargin,
+                        backgroundColor: color,
+                    }}
+                >
+                    <FiExternalLink
+                        color='#f5f5f5'
+                        style={{ paddingRight: 7 }}
+                    />
+                    <h3 style={{ color: '#f5f5f5' }}>site</h3>
+                </div>
+            </a>
+        ) : null;
+
+        const codeButton = code ? (
+            <a href={code}>
+                <div
+                    className='projectcard-code'
+                    style={{
+                        backgroundColor: color,
+                    }}
+                >
+                    <FaCode color='#f5f5f5' style={{ paddingRight: 7 }} />
+                    <h3 style={{ color: '#f5f5f5' }}>code</h3>
+                </div>
+            </a>
+        ) : null;
+
         const ProjectCard = withStyles({
             root: {
                 height: 550,
                 width: 1000,
-                backgroundColor: DARK_PINK,
+                backgroundColor,
                 boxShadow: '20px 20px 20px black',
                 display: 'flex',
                 borderRadius: 5,
@@ -47,63 +101,24 @@ class Projects extends React.Component {
             <div className='projects-container'>
                 <Header page='projects' />
                 <div className='projects-content'>
-                    {/*
-                    <div className='projects-title'>
-                        <h1>
-                            here are some things
-                            <br /> i've worked on
-                        </h1>
-                    </div> */}
                     <div className='projectcard-container'>
                         <ProjectCard>
                             <div className='projectcard-image'>
-                                <img src={Gradient} />
+                                <img src={image} />
                             </div>
                             <InfoCard>
-                                <h2 style={{ marginBottom: 30 }}>
-                                    gradient.png
-                                </h2>
+                                <h2 style={{ marginBottom: 30 }}>{name}</h2>
                                 <div className='projectcard-content'>
-                                    <h3 style={{ color: DARK_PINK }}>type: </h3>
-                                    <p>static web page</p>
-                                    <h3 style={{ color: DARK_PINK }}>stack:</h3>
-                                    <p>React</p>
-                                    <h3 style={{ color: DARK_PINK }}>
-                                        description:
-                                    </h3>
-                                    <p>
-                                        Nulla mi ultrices risus lectus vel, elit
-                                        nulla etiam in tristique nec in, vivamus
-                                        eget ut, id sapien integer. Sed semper
-                                        ridiculus facilisis ante. Nulla libero
-                                        ut vestibulum vitae quam, justo
-                                        consequat nunc orci. Nisl interdum
-                                        aliquet elementum pellentesque venenatis
-                                        quis.
-                                    </p>
+                                    <h3 style={{ color }}>type: </h3>
+                                    <p>{type}</p>
+                                    <h3 style={{ color }}>stack:</h3>
+                                    <p>{stack}</p>
+                                    <h3 style={{ color }}>description:</h3>
+                                    <p>{description}</p>
                                 </div>
                                 <div className='projectcard-buttons'>
-                                    <div
-                                        className='projectcard-demo'
-                                        style={{ marginRight: 20 }}
-                                    >
-                                        <FiExternalLink
-                                            color={DARK_PINK}
-                                            style={{ paddingRight: 7 }}
-                                        />
-                                        <h3 style={{ color: DARK_PINK }}>
-                                            demo
-                                        </h3>
-                                    </div>
-                                    <div className='projectcard-code'>
-                                        <FaCode
-                                            color={DARK_PINK}
-                                            style={{ paddingRight: 7 }}
-                                        />
-                                        <h3 style={{ color: DARK_PINK }}>
-                                            code
-                                        </h3>
-                                    </div>
+                                    {siteButton}
+                                    {codeButton}
                                 </div>
                                 <h2></h2>
                             </InfoCard>
--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,82 @@
-export const DARK_GRAY = "#333333";
+import Expresso from './Assets/expresso.png';
+import Dayz from './Assets/dayz.png';
+import Gradient from './Assets/gradient.png';
+import ColorCharts from './Assets/colorcharts.png';
+import DriversEd from './Assets/driversed.gif';
 
-export const DARK_PINK = "#FF7C7C";
+export const DARK_GRAY = '#333333';
 
-export const LIGHT_PINK = "#FFB6B9";
+export const DARK_PINK = '#FF7C7C';
 
-export const BLUE = "#8DEAFF";
+export const LIGHT_PINK = '#FFB6B9';
 
-export const YELLOW = "#FFE084";
+export const BLUE = '#8DEAFF';
 
-export const LIGHT_GRAY = "#D9D9D9"
+export const YELLOW = '#FFE084';
+
+export const LIGHT_GRAY = '#D9D9D9';
+
+export const projectInfo = [
+    {
+        backgroundColor: DARK_PINK,
+        color: DARK_PINK,
+        image: Gradient,
+        name: 'gradient.png',
+        type: 'static web page',
+        stack: 'React',
+        description:
+            ' Nulla mi ultrices risus lectus vel, elit nulla etiam in tristique nec in, vivamu eget ut, id sapien integer. Sed semper ridiculus facilisis ante. Nulla libero ut vestibulum vitae quam, justo consequat nunc orci. Nisl interdum aliquet elementum pellentesque venenatis quis.',
+        site: 'https://gradient-png.netlify.app/',
+        code: 'https://github.com/karenying/gradient',
+    },
+    {
+        backgroundColor: LIGHT_PINK,
+        color: '#E9A5A8',
+        image: Dayz,
+        name: 'dayz',
+        type: 'full stack web app',
+        stack: 'MongoDB + Express + React',
+        description:
+            ' Nulla mi ultrices risus lectus vel, elit nulla etiam in tristique nec in, vivamu eget ut, id sapien integer. Sed semper ridiculus facilisis ante. Nulla libero ut vestibulum vitae quam, justo consequat nunc orci. Nisl interdum aliquet elementum pellentesque venenatis quis.',
+        site: 'http://dayz-app.herokuapp.com/',
+        code: 'https://github.com/karenying/dayz',
+    },
+    {
+        backgroundColor: YELLOW,
+        color: '#DCC272',
+        image: Expresso,
+        name: 'expresso',
+        type: 'full stack web app',
+        stack: 'MySQL + Flask + React',
+        description:
+            ' Nulla mi ultrices risus lectus vel, elit nulla etiam in tristique nec in, vivamu eget ut, id sapien integer. Sed semper ridiculus facilisis ante. Nulla libero ut vestibulum vitae quam, justo consequat nunc orci. Nisl interdum aliquet elementum pellentesque venenatis quis.',
+        site: 'http://coffeeclub.princeton.edu/',
+        code: null,
+    },
+
+    {
+        backgroundColor: BLUE,
+        color: '#72BECF',
+        image: ColorCharts,
+        name: 'color charts',
+        type: 'Chrome extension',
+        stack: 'JS',
+        description:
+            ' Nulla mi ultrices risus lectus vel, elit nulla etiam in tristique nec in, vivamu eget ut, id sapien integer. Sed semper ridiculus facilisis ante. Nulla libero ut vestibulum vitae quam, justo consequat nunc orci. Nisl interdum aliquet elementum pellentesque venenatis quis.',
+        site: null,
+        code: 'https://github.com/karenying/color-charts',
+    },
+
+    {
+        backgroundColor: LIGHT_GRAY,
+        color: '#9A9A9A',
+        image: DriversEd,
+        name: "driver's ed",
+        type: 'three.js game',
+        stack: 'JS',
+        description:
+            ' Nulla mi ultrices risus lectus vel, elit nulla etiam in tristique nec in, vivamu eget ut, id sapien integer. Sed semper ridiculus facilisis ante. Nulla libero ut vestibulum vitae quam, justo consequat nunc orci. Nisl interdum aliquet elementum pellentesque venenatis quis.',
+        site: 'https://karenying.com/drivers-ed/',
+        code: 'https://github.com/karenying/drivers-ed/',
+    },
+];
