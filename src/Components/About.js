c1478910ecd63c3e349d1105d5ac611e7db9b57a
Karen Ying <karenying7@gmail.com>
2020-05-25 11:36:12 -0400

    Add progressive image loading

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,91 @@
 import React from 'react';
 import '../Styles/About.css';
 import Header from './Header';
-// import Karen from '../Assets/karen.png';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Karen from '../Assets/karen.png';
+import MiniKaren from '../Assets/mini-karen.png';
+import ProgressiveImage from 'react-progressive-image';
 
-class About extends React.Component {
-    render() {
-        return (
-            <>
-                <div className='about-container'>
-                    <Header page='about' />
-                </div>
-            </>
-        );
-    }
+function About() {
+    const useStyles = makeStyles((theme) => ({
+        aboutGrid: {
+            textAlign: 'left',
+            maxWidth: 1000,
+            [theme.breakpoints.up('md')]: {
+                padding: '100px 0px',
+            },
+            [theme.breakpoints.down('md')]: {
+                padding: '10px 50px',
+            },
+        },
+        image: {
+            display: 'flex',
+            alignItems: 'center',
+            width: '100%',
+            [theme.breakpoints.up('md')]: {
+                justifyContent: 'left',
+            },
+            [theme.breakpoints.down('md')]: {
+                paddingBottom: '10px',
+                justifyContent: 'center',
+            },
+        },
+    }));
+
+    const classes = useStyles();
+
+    return (
+        <>
+            <div className='about-container'>
+                <Header page='about' />
+                <Grid className={classes.aboutGrid} container>
+                    <Grid item sm={12} md={4} className={classes.image}>
+                        <ProgressiveImage
+                            src={Karen}
+                            placeholder={MiniKaren}
+                            delay={300}
+                        >
+                            {(src, loading) => (
+                                <img
+                                    style={{ opacity: loading ? 0.8 : 1 }}
+                                    src={src}
+                                    alt='karen'
+                                />
+                            )}
+                        </ProgressiveImage>
+                    </Grid>
+                    <Grid item sm={12} md={8}>
+                        <h2>Karen Ying</h2>
+                        <p>
+                            Blandit nullam nisl, morbi duis. Amet odio, amet
+                            scelerisque pede volutpat ipsum, lorem nisl proin
+                            pharetra fusce mollis, ut sed morbi nullam semper
+                            eget, sem ut urna. Ornare mauris aliquip
+                            scelerisque, semper velit. Fringilla blandit nullam
+                            nibh, risus porta sagittis, aliquam eu elit sem,
+                            sociis vitae vel sollicitudin tellus erat turpis.
+                            Cum feugiat amet, at volutpat et orci amet odio
+                            tempor, praesent morbi fermentum arcu dui duis,
+                            sodales nam vitae fringilla elementum. Ultricies
+                            nibh lectus proin scelerisque, aenean ac dapibus
+                            consectetuer aut bibendum, elit morbi ut tortor nunc
+                            nunc non, in donec mauris a molestie amet. Purus at
+                            justo posuere eu, quis nulla ipsum ut aenean,
+                            dapibus nullam tenetur aliquam in aliquam in. Varius
+                            pariatur class, etiam velit fusce consectetuer magna
+                            sem nibh. Felis sit pellentesque augue aliquet
+                            varius est. In lorem, vitae suspendisse aliquet hac
+                            at nonummy, adipiscing non, felis nam. Magna cras
+                            quam nec etiam, nulla quis in, sollicitudin ante
+                            scelerisque tellus. Fermentum tincidunt, nulla
+                            velit, proin maecenas nibh egestas.
+                        </p>
+                    </Grid>
+                </Grid>
+            </div>
+        </>
+    );
 }
 
 export default About;
--- src/Components/FooterIcon.js
+++ src/Components/FooterIcon.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../Styles/FooterIcon.css';
 import { FaChevronUp } from 'react-icons/fa';
 import { DARK_PINK } from '../constants';
+import Hidden from '@material-ui/core/Hidden';
 
 class FooterIcon extends React.Component {
     render() {
         const { clickFunction } = this.props;
 
         return (
-            <>
+            <Hidden xsDown>
                 <div className='footer-icon-container'>
                     <div
                         className='footer-icon-content'
@@ -21,7 +22,7 @@ class FooterIcon extends React.Component {
                         />
                     </div>
                 </div>
-            </>
+            </Hidden>
         );
     }
 }
