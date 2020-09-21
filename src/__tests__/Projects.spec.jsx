4f7472bbcc3aa5ad2e6aaa72d151cbcf7839bf74
Karen Ying <karenying7@gmail.com>
2020-09-20 22:05:37 -0400

    Attempt to fix tests

--- src/__tests__/Projects.spec.jsx
+++ src/__tests__/Projects.spec.jsx
@@ -4,24 +4,29 @@ import { shallow, mount } from "enzyme";
 import Projects from "../Components/Projects";
 
 describe("Projects", () => {
-  it("renders the two arrows", () => {
-    const wrapper = shallow(<Projects />);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = mount(<Projects />);
+  });
+
+  it("renders the two arrows", () => {
     expect(wrapper.find("Arrow").length).toEqual(2);
   });
 
   it("renders gradient.png as the first slide", () => {
-    const wrapper = shallow(<Projects />);
     expect(wrapper.find("h2").text()).toBe("gradient.png");
   });
 
   it("renders dayz when the right arrow is clicked", () => {
-    const wrapper = mount(<Projects />);
-    /*     wrapper.find(".arrow-container").at(1).simulate("click");
-    wrapper.update();
-    wrapper.simulate("keydown", { keyCode: 39 });
-    expect(wrapper.find("h2").text()).toBe("dayz"); */
+    wrapper.find(".arrow-container").at(0).simulate("click");
+    // wrapper.simulate("keydown", { keyCode: 39 });
+    // expect(wrapper.find("h2").text()).toBe("dayz");
   });
 
-  it("renders gradient.png when the left arrow is clicked", () => {});
+  it("renders gradient.png when the left arrow is clicked", () => {
+    wrapper.find(".arrow-container").at(0).simulate("click");
+    wrapper.find(".arrow-container").at(1).simulate("click");
+    expect(wrapper.find("h2").text()).toBe("gradient.png");
+  });
 });
